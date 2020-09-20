0b3b49fbc4d914d5dbb5dfc728aa1e235148f4e9
wangfeng00390438 <wang_feng7@want-want.com>
2020-09-20 16:28:29 +0800

    add normalize.css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "normalize.css/normalize.css"; // A modern alternative to CSS resets
 
 createApp(App)
   .use(store)
